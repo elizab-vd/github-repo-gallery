07e3f7f8bd93d4210ea09ffe889b99f9e8b54249
Eliza B <82332815+elizab-vd@users.noreply.github.com>
2021-07-20 13:46:44 -0400

    Added function to display GitHub profile info, called in async

--- js/script.js
+++ js/script.js
@@ -6,8 +6,27 @@ const gitHubInfo = async function () {
     const res = await fetch (
         `https://api.github.com/users/${username}`
     );
-    const projects = await res.json();
-    console.log(projects);
+    const userData = await res.json();
+    console.log(userData);
+    showGitHubInfo(userData);
 };
 
 gitHubInfo();
+
+const showGitHubInfo = function (userData) {
+     const userInfo = document.createElement("div");
+     userInfo.classList.add("user-info");
+     userInfo.innerHTML = `
+     <figure>
+     <img alt="user avatar" src=${userData.avatar_url} />
+   </figure>
+   <div>
+     <p><strong>Name:</strong> ${userData.name}</p>
+     <p><strong>Bio:</strong> ${userData.bio}</p>
+     <p><strong>Location:</strong> ${userData.location}</p>
+     <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
+   </div>
+   `;
+   profileInfo.append(userInfo);
+     
+}
