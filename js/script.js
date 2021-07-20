f08765a5a24adc5c41b7953496e0194d68eb2ecc
Eliza B <82332815+elizab-vd@users.noreply.github.com>
2021-07-20 13:28:36 -0400

    Created async function to access the GitHub API

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 const profileInfo = document.querySelector(".overview");
 //GitHub username
 const username = "elizab-vd";
+const gitHubInfo = async function () {
+    const res = await fetch (
+        `https://api.github.com/users/${username}`
+    );
+    const projects = await res.json();
+    console.log(projects);
+};
+
+gitHubInfo();
